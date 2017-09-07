910b6902c059f4b7ad8393a16117333db97f5790
Hyunju Jeong <0106grace@naver.com>
2017-09-07 11:54:05 +0900

    Controller 추가 - 화이트라벨 안녕~

---
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.nmb2017.springmvc4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+		return "Hello, World?!";
+	}
+}
